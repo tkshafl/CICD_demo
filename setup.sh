c7594fa98d3a50935878967617981cd12eaeedcd
Tushar Sha <tusharksha@gmail.com>
2023-08-25 13:25:30 -0400

    cicd pipeline updated main.yml

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 python3 -m venv /home/tusha/cicd_demo/cdci-venv
 source /home/tusha/cicd_demo/cdci-venv/bin/activate
 pip install -r /home/tusha/cicd_demo/requirement.txt
-sudo supervisorctl restart cicd-demogit status
+sudo supervisorctl restart cicd_demo 
